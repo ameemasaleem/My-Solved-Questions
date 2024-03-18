1b84129feb041fb47ecc3771ce7063cf14da8644
Ameema Saleem <161148265+ameemasaleem@users.noreply.github.com>
2024-03-18 03:01:45 -0700

    Add files via upload

---
+++ Question2.js
@@ -0,0 +1,2 @@
+console.log("Hello Ameema");
+console.log("Hello Ameema,would you like to learn some typescript today");
---
+++ Question2.ts
@@ -0,0 +1,4 @@
+console.log("Hello Ameema");
+
+console.log("Hello Ameema,would you like to learn some typescript today");
+
